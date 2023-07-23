55ef51eec69e89e91a3d5300d896f1c14a070856
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2023-07-22 20:11:36 -0500

    Update

--- env.py
+++ env.py
@@ -3,8 +3,11 @@ from dataclasses import field
 from heapq import heappop
 from heapq import heappush
 from pprint import pprint
+from typing import Optional
+
+import gymnasium as gym
 import numpy as np
-from numpy.random import default_rng
+
 
 @dataclass
 class Place:
@@ -19,120 +22,241 @@ class Truck:
     travel_remaining: float = 0
     work_place: tuple[int, int] | None = None
     work_remaining: float = 0
-    status : str = "work"
+    status: str = "work"
 
 
-rng = default_rng(42)
-num_trucks = 6
-num_places =(3, 3)
-list_place = [[Place() for _ in range(num_places[i])] for i in range(2)]
-list_truck = [Truck() for _ in range(num_trucks)]
-capacity = 20 # t
-distance = 5 # km
-travel_speed = 30 / 60 # km/min
-work_speed = 20 # t/min
-work_sigma = 0 # t
-travel_sigma = 0
+class MineralEnv(gym.Env):
+    metadata = {
+        "render_modes": ["human", "rgb_array"],
+        "render_fps": 4,
+    }
 
+    def __init__(
+        self,
+        render_mode: Optional[str] = None,
+    ):
+        self.render_mode = render_mode
 
-# Initialization
-for i_truck in range(num_trucks):
-    # truck i travel to shovel i%num_places
-    list_truck[i_truck].travel_destination = (0, i_truck % num_places[0])
-    list_truck[i_truck].travel_origin = (1, i_truck % num_places[0])
-    list_truck[i_truck].travel_remaining = 2
-    list_truck[i_truck].status = "travel"
+        self.num_trucks = 10
+        self.num_places = (3, 3)
+        self.capacity = 1  # t
+        self.distance = 2*2**0.5  # km
+        self.travel_speed = 60 / 60  # km/min
+        self.work_speed = 1  # t/min
+        self.work_sigma = 0  # t
+        self.travel_sigma = 0  # km
 
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=self.num_trucks,
+            shape=(2 * self.num_places[0] + self.num_places[1],),
+        )
+        self.action_space = gym.spaces.Discrete(self.num_places[0])
 
-for _ in range(20):
-    # Encontrar la lista de tiempos faltantes
-    list_time_remaining = []
-    for i_truck in range(num_trucks):
-        truck = list_truck[i_truck]
-        if truck.status == 'work':
-            remaining = truck.work_remaining
-            speed = work_speed
-            sigma = work_sigma
-        elif truck.status == 'travel':
-            remaining = truck.travel_remaining
-            speed = travel_speed
-            sigma = travel_sigma
+        if self.render_mode == "human":
+            import pygame
 
-        mean = remaining / speed
+            pygame.init()
+            self.screen = pygame.display.set_mode((600, 500))
+            pygame.font.init()
+            self.font = pygame.font.Font(None, 20)
+            self.clock = pygame.time.Clock()
 
-        try:
-            scale = remaining ** 2 / sigma ** 2
-        except ZeroDivisionError:
-            time_remaining = mean
-        else:
-            if mean == 0:
-                time_remaining = 0
-            else:
-                time_remaining = rng.wald(mean, scale)
+    def reset(self, seed=None):
+        super().reset(seed=seed)
+        self.list_place = [
+            [Place() for _ in range(self.num_places[i])] for i in range(2)
+        ]
+        self.list_truck = [Truck() for _ in range(self.num_trucks)]
 
+        # Initialization
+        for i_truck in range(self.num_trucks):
+            # truck i travel to shovel i%num_places
+            self.list_truck[i_truck].travel_destination = (
+                0,
+                i_truck % self.num_places[0],
+            )
+            self.list_truck[i_truck].travel_origin = (
+                1,
+                i_truck % self.num_places[0],
+            )
+            self.list_truck[i_truck].travel_remaining = 2 + 0.1 * i_truck
+            self.list_truck[i_truck].status = "travel"
 
-        list_time_remaining.append(time_remaining)
-        
-    # Encontrar el camion que hace el evento m'as proximo
-    id_truck_first = min(range(num_trucks), key=list_time_remaining.__getitem__)
-    remaining_first = list_time_remaining[id_truck_first]
-    print()
-    print(f"{list_time_remaining}")
-    print(f"{[truck.status for truck in list_truck]}")
-    print(f"{id_truck_first=} {remaining_first=}")
-    truck_first = list_truck[id_truck_first]
+        self.worker = self._worker()
+        next(self.worker)
 
+        if self.render_mode == "human":
+            self.render()
 
-    # update the others trucks
-    for i_truck in range(num_trucks):
-        if i_truck == id_truck_first:
-            continue
-        
-        truck = list_truck[i_truck]
-        if truck.status == 'work':
-            while True:
-                w_t = rng.normal(0, np.sqrt(remaining_first))
-                displacement = work_speed * remaining_first + work_sigma * w_t
-                if truck.work_remaining >= displacement:
-                    truck.work_remaining -= displacement
-                    break
-        elif truck.status == 'travel':
-            while True:
-                w_t = rng.normal(0, np.sqrt(remaining_first))
-                displacement = travel_speed * remaining_first + travel_sigma * w_t
-                if truck.travel_remaining >= displacement:
-                    truck.travel_remaining -= displacement
-                    break
-        elif truck.status == 'queue':
-            pass
+        return "", {}
 
-    # update the first_truck
-    if truck_first.status == 'work':
-        action = int(input(f'A donde mando el camion {id_truck_first}: '))
+    def step(self, action: int):
+        self.worker.send(action)
 
-        truck_first.status = 'travel'
-        truck_first.travel_origin = truck_first.travel_destination
-        truck_first.travel_destination = (1 - truck_first.travel_destination[0], action)
-        truck_first.travel_remaining = distance
+        if self.render_mode == "human":
+            self.render()
 
-        place = list_place[truck_first.travel_destination[0]][truck_first.travel_destination[1]]
-        place.current_truck = None
+        return None, None, False, False, {}
 
-        if place.queue:
-            i_truck_next = place.queue.pop(0)
-            truck_next = list_truck[i_truck_next]
-            truck_next.status = 'work'
-            truck_next.work_place = truck_next.travel_destination
-            truck_next.work_remaining = capacity
-            place.current_truck = i_truck_next
+    def render(self):
+        import pygame
 
-    elif truck_first.status == 'travel':
-        place = list_place[truck_first.travel_destination[0]][truck_first.travel_destination[1]]
-        if place.current_truck is not None:
-            truck_first.status = 'queue'
-            place.queue.append(id_truck_first)
-        else:
-            truck_first.status = "work"
-            truck_first.work_place = truck_first.travel_destination
-            truck_first.work_remaining = capacity
-            place.current_truck = id_truck_first
+        self.screen.fill("black")
+        for i in range(2):
+            for j in range(self.num_places[i]):
+                pos = pygame.Vector2(
+                    300 + (2 * i - 1) * 280,
+                    50 + 400 * (j + 1) / (self.num_places[i] + 1),
+                )
+                pygame.draw.circle(self.screen, "dark red", pos, 10)
+
+        for i_truck in range(self.num_trucks):
+            truck = self.list_truck[i_truck]
+            if truck.status == "travel":
+                pos = (
+                    truck.travel_remaining
+                    * pygame.Vector2(
+                        300 + (2 * truck.travel_origin[0] - 1) * 280,
+                        50 + 400 * (truck.travel_origin[1] + 1) / (self.num_places[truck.travel_origin[0]] + 1),
+                    )
+                    + (self.distance - truck.travel_remaining)
+                    * pygame.Vector2(
+                        300 + (2 * truck.travel_destination[0] - 1) * 280,
+                        50 + 400 * (truck.travel_destination[1] + 1) / (self.num_places[truck.travel_destination[0]] + 1),
+                    )
+                ) / self.distance
+                print(f"{pos=}")
+                pygame.draw.circle(self.screen, "white", pos, 5)
+
+        pygame.display.flip()
+        pygame.event.pump()
+        pygame.display.update()
+
+    def close(self):
+        if hasattr(self, "screen"):
+            import pygame
+
+            pygame.display.quit()
+            pygame.quit()
+
+    def _worker(self):
+        while True:
+            # Find all remaining times
+            list_time_remaining = []
+            for i_truck in range(self.num_trucks):
+                truck = self.list_truck[i_truck]
+
+                if truck.status == "queue":
+                    time_remaining = float("inf")
+                else:
+                    if truck.status == "work":
+                        remaining = truck.work_remaining
+                        speed = self.work_speed
+                        sigma = self.work_sigma
+                    elif truck.status == "travel":
+                        remaining = truck.travel_remaining
+                        speed = self.travel_speed
+                        sigma = self.travel_sigma
+
+                    mean = remaining / speed
+
+                    try:
+                        scale = remaining**2 / sigma**2
+                    except ZeroDivisionError:
+                        time_remaining = mean
+                    else:
+                        if mean == 0:
+                            time_remaining = 0
+                        else:
+                            time_remaining = self.np_random.wald(mean, scale)
+
+                list_time_remaining.append(time_remaining)
+
+            # Find the truck with the lowest remaining time (truck_first)
+            id_truck_first = min(
+                range(self.num_trucks), key=list_time_remaining.__getitem__
+            )
+            remaining_first = list_time_remaining[id_truck_first]
+            remaining_first = max(remaining_first, 0)
+            print()
+            print(f"{list_time_remaining}")
+            print(f"{[truck.status for truck in self.list_truck]}")
+            print([p.queue for ps in self.list_place for p in ps])
+            print([p.current_truck for ps in self.list_place for p in ps])
+            print(f"{id_truck_first=} {remaining_first=}")
+            truck_first = self.list_truck[id_truck_first]
+
+            # update the others trucks
+            for i_truck in range(self.num_trucks):
+                if i_truck == id_truck_first:
+                    continue
+
+                truck = self.list_truck[i_truck]
+                if truck.status == "work":
+                    while True:
+                        w_t = self.np_random.normal(
+                            0, np.sqrt(remaining_first)
+                        )
+                        displacement = (
+                            self.work_speed * remaining_first
+                            + self.work_sigma * w_t
+                        )
+                        if truck.work_remaining >= displacement - 1e-8:
+                            truck.work_remaining -= displacement
+                            break
+                elif truck.status == "travel":
+                    while True:
+                        w_t = self.np_random.normal(
+                            0, np.sqrt(remaining_first)
+                        )
+                        displacement = (
+                            self.travel_speed * remaining_first
+                            + self.travel_sigma * w_t
+                        )
+                        if truck.travel_remaining >= displacement - 1e-8:
+                            truck.travel_remaining -= displacement
+                            break
+                elif truck.status == "queue":
+                    pass
+
+            # update the first_truck
+            if truck_first.status == "work":
+                action = yield
+
+                truck_first.status = "travel"
+                truck_first.travel_origin = truck_first.travel_destination
+                truck_first.travel_destination = (
+                    1 - truck_first.travel_destination[0],
+                    action,
+                )
+                truck_first.travel_remaining = self.distance
+
+                place = self.list_place[truck_first.work_place[0]][
+                    truck_first.work_place[1]
+                ]
+                place.current_truck = None
+
+                if place.queue:
+                    i_truck_next = place.queue.pop(0)
+                    truck_next = self.list_truck[i_truck_next]
+                    truck_next.status = "work"
+                    truck_next.work_place = truck_next.travel_destination
+                    truck_next.work_remaining = self.capacity
+                    place.current_truck = i_truck_next
+
+            elif truck_first.status == "travel":
+                place = self.list_place[truck_first.travel_destination[0]][
+                    truck_first.travel_destination[1]
+                ]
+                if place.current_truck is not None:
+                    truck_first.status = "queue"
+                    place.queue.append(id_truck_first)
+                else:
+                    truck_first.status = "work"
+                    truck_first.work_place = truck_first.travel_destination
+                    truck_first.work_remaining = self.capacity
+                    place.current_truck = id_truck_first
+
+    def _get_obs(self):
+        return (None,), {}
--- main_random.py
+++ main_random.py
@@ -1,25 +1,24 @@
 from itertools import count
-from environment import MineralEnv
+from env import MineralEnv
 
 import gymnasium as gym
 
 
-env = MineralEnv(num_queues=(3, 3), num_trucks=5, num_blocks=100, render_mode="human")
+env = MineralEnv(render_mode="human")
+#env = MineralEnv()
 
 for episode in count():
     observation, info = env.reset(seed=42)
 
     for t in count():
         # Select an action
-        #action = env.action_space.sample()
-        action = int(input())
+        action = env.action_space.sample()
+        #action = int(input("Ingrese acción: "))
 
         # Perform action in the enviroment
         next_observation, reward, terminated, truncated, info = env.step(
             action
         )
-
-        
 
         if terminated or truncated:
             break
